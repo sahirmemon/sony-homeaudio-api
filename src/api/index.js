7ade818ff4a65c2fd8aa77da91d19a91d710f67d
Sahir Memon <sahir.memon@gmail.com>
2018-04-03 22:02:02 -0400

    Added in some API calls to soundbar

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,201 @@
-import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
+import request from 'request-promise';
+import { version } from '../../package.json';
+import { soundbarURL } from '../config.json';
+
 
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	api.get('/system-information', (req, res) => {
 
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
+		// Get soundbar's system information
+		var options = {
+	    method: 'POST',
+	    uri: `${soundbarURL}/sony/system`,
+	    body: {
+				method: 'getSystemInformation',
+				id: 65,
+				params: [],
+				version: '1.4'
+	    },
+	    json: true
+		};
+
+		request(options)
+	    .then((parsedBody) => {
+	    	res.json({ parsedBody });
+	    })
+	    .catch((err) => {
+	    	res.status(500).json({ err });
+	    });
+	});
+
+	api.get('/power-status', (req, res) => {
+
+		// Get soundbar's power status
+		var options = {
+			method: 'POST',
+			uri: `${soundbarURL}/sony/system`,
+			body: {
+				method: 'getPowerStatus',
+				id: 50,
+				params: [],
+				version: '1.1'
+			},
+			json: true
+		};
+
+		request(options)
+			.then((parsedBody) => {
+				res.json({ parsedBody });
+			})
+			.catch((err) => {
+				res.status(500).json({ err });
+			});
+	});
+
+	api.get('/power-on', (req, res) => {
+
+		// Turn on the soundbar
+		var options = {
+			method: 'POST',
+			uri: `${soundbarURL}/sony/system`,
+			body: {
+				method: 'setPowerStatus',
+				id: 55,
+				params: [
+					{
+						status: "active"
+					}
+				],
+				version: '1.1'
+			},
+			json: true
+		};
+
+		request(options)
+			.then((parsedBody) => {
+				res.json({ parsedBody });
+			})
+			.catch((err) => {
+				res.status(500).json({ err });
+			});
+	});
+
+	api.get('/power-off', (req, res) => {
+
+		// Turn off the soundbar
+		var options = {
+			method: 'POST',
+			uri: `${soundbarURL}/sony/system`,
+			body: {
+				method: 'setPowerStatus',
+				id: 55,
+				params: [
+					{
+						status: "off"
+					}
+				],
+				version: '1.1'
+			},
+			json: true
+		};
+
+		request(options)
+			.then((parsedBody) => {
+				res.json({ parsedBody });
+			})
+			.catch((err) => {
+				res.status(500).json({ err });
+			});
+	});
+
+	api.get('/switch-to-analog', (req, res) => {
+
+		// Turn off the soundbar
+		var options = {
+			method: 'POST',
+			uri: `${soundbarURL}/sony/avContent`,
+			body: {
+				method: 'setActiveTerminal',
+				id: 13,
+				params: [
+					{
+						active: 'active',
+						uri: 'extInput:line'
+					}
+				],
+				version: '1.0'
+			},
+			json: true
+		};
+
+		request(options)
+			.then((parsedBody) => {
+				res.json({ parsedBody });
+			})
+			.catch((err) => {
+				res.status(500).json({ err });
+			});
+	});
+
+	api.get('/switch-to-appletv', (req, res) => {
+
+		// Turn off the soundbar
+		var options = {
+			method: 'POST',
+			uri: `${soundbarURL}/sony/avContent`,
+			body: {
+				method: 'setActiveTerminal',
+				id: 13,
+				params: [
+					{
+						active: 'active',
+						uri: 'extInput:hdmi'
+					}
+				],
+				version: '1.0'
+			},
+			json: true
+		};
+
+		request(options)
+			.then((parsedBody) => {
+				res.json({ parsedBody });
+			})
+			.catch((err) => {
+				res.status(500).json({ err });
+			});
+	});
+
+	api.get('/switch-to-xbox', (req, res) => {
+
+		// Turn off the soundbar
+		var options = {
+			method: 'POST',
+			uri: `${soundbarURL}/sony/avContent`,
+			body: {
+				method: 'setActiveTerminal',
+				id: 13,
+				params: [
+					{
+						active: 'active',
+						uri: 'extInput:hdmi?port=1'
+					}
+				],
+				version: '1.0'
+			},
+			json: true
+		};
+
+		request(options)
+			.then((parsedBody) => {
+				res.json({ parsedBody });
+			})
+			.catch((err) => {
+				res.status(500).json({ err });
+			});
 	});
 
 	return api;
